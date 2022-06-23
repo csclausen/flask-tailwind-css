11577eb71bdd4101771b9f24772dea1b4968e5ce
csclausen <csclausen92@gmail.com>
2022-06-22 19:25:16 -0600

    Add dotenv package Add the dotenv package to requirements. Also load the SECRETY_KEY from a .env file and raise exception if not found.

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,4 +1,6 @@
 import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 
 from flask import Flask, render_template
 
@@ -7,10 +9,18 @@ from .views import auth
 
 
 def create_app(test_config=None):
+    # look for .env in this directory
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    if SECRET_KEY is None:
+        raise Exception('Add SECRET_KEY to .env file')
+
     # create and configure app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='big secret',
+        SECRET_KEY=SECRET_KEY,
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
